023d9eb2ea85bfe933c2b94f7f3fb0e1902544e9
Ali ODABAŞI <aliodabas_a@hotmail.com>
2022-12-13 19:11:36 +0300

    first initialization

---
+++ script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // when there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  }
+
+  // when the player wins
+  else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  }
+
+  // when guess is too high
+  else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High!';
+      score--; // score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+
+  // when guess is too low
+  else if (guess < secretNumber)
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High!';
+      score--; // score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost!';
+      document.querySelector('.score').textContent = 0;
+    }
+});
